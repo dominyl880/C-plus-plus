d4ab569ca04b464b2afde74a52d2ae03721179cc
srp020 <139586574+srp020@users.noreply.github.com>
2023-10-07 14:16:15 +0700

    Add files via upload

---
+++ header.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <cctype>
+#include <string.h>
+#include <iostream>
+
+void headerTable()
+{
+  printf("===============================|   General Store   |===============================\n");
+}
