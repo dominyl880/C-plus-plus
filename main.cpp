c03e25d138373423ed4ac59fffdc9c9b9d92ca63
YANiS <dominyl880@gmail.com>
2023-10-07 15:17:08 +0700

    55

--- main.cpp
+++ main.cpp
@@ -1,14 +1,4 @@
-#include<iostream>//cin cout
-using namespace std; //std::
-
-// include อื่นๆ
-#include <stdio.h>//puts
-#include <stdlib.h>
-#include <conio.h>
-#include <cctype>
-#include <string.h>
-
-// #include "head.cpp"
+#include "head.cpp"
 
 //ไฟล์ sale
 #include "sale.cpp" 
