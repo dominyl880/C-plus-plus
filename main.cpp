9014eed8602eb7fd690953e1a4203ae194f935c6
YANiS <dominyl880@gmail.com>
2023-10-08 01:46:10 +0700

    5

--- main.cpp
+++ main.cpp
@@ -8,15 +8,17 @@
 
 // ================================Type Menu==========================
 void typeMenu(){
+      string This,ID;
       char sell_t;
       int t,t2;
+      const int columnWidth = 12;
       string Type_list[10][2]={{"T1","Stationery"},
                                 {"T2","Food"},
                                 {"T3","Drink"}
                                 };
       do{
           headerTable();  //แสดง =======
-          cout<<setw(54)<<"----------Type Menu----------"<<endl;
+          cout<<setw(54)<<"---------- Type Menu ----------"<<endl;
           cout<<setw(56)<<"Press [L] to Show List Type"<<endl;
           cout<<setw(53)<<"Press [S] to Search Type"<<endl;
           cout<<setw(50)<<"Press [A] to Add Type"<<endl;  
@@ -26,19 +28,75 @@ void typeMenu(){
           cin>>sell_t;
           sell_t = toupper(sell_t);
           if(sell_t=='L'){ 
-              cout<<"show_list_in T"<<endl;
+              cout<<setw(60)<<"--------------- List Type ---------------"<<endl;
+              cout<<"-------------------------------------------------------------------------------"<<endl;
+              cout<<setw(8)<<"ID"<<setw(14)<<"Name"<<endl;
+              cout<<"-------------------------------------------------------------------------------"<<endl;
 
                  for(int i=0;i<3;i++){
                       for(int i2=0;i2<2;i2++){
-                          cout<<setw(35)<<Type_list[i][i2]<<"   ";
+                          cout<<left<<setw(columnWidth)<<Type_list[i][i2]<<" | ";
                       }
                       //cout<<"------------------------------";
                       cout<<endl;
                   }
           }else if(sell_t=='S'){
-            // searchType();
+            cout<<setw(65)<<"--------------- Search Type ---------------"<<endl;
+            cout<<"Enter ID : "; cin>>ID;
+
+
+            for(int m=0; m<100;m++){
+                for(int u=0; u<3;u++){
+                    if(Type_list[m][u]==""){
+                        break; //ข้ามไป
+                    } 
+                    t=m+1;
+                }
+                cout<<endl;
+            }
+            for(int i=0;i<t;i++){
+                for(int i2=0;i2<3;i2++){
+                    if(ID==Type_list[i][i2]){
+                        This=Type_list[i][i2];
+                        t=i; 
+                    }
+                }  
+            }  
+            if(This==""){
+                cout<<"Not Found!"<<endl;
+            }else{
+                    cout<<"ID : "<<Type_list[t][0]<<endl;
+                    cout<<"Name : "<<Type_list[t][1]<<endl;
+                    cout<<endl; 
+            }
+            This="";
+            cout<<"-------------------------------------------------------------------------------"<<endl;
+
           }else if(sell_t=='A'){
-            // AddType();
+            cout<<setw(60)<<"--------------- Add Type ---------------"<<endl;
+            for(int m=0; m<100;m++){
+                for(int u=0; u<3;u++){
+                    // cout<<product[m][u]<<"   ";
+                    
+                    if(Type_list[m][u]==""){
+                        break; //ข้ามไป
+                    } 
+                    t=m+1;
+                }
+                // cout<<endl;
+            }
+                cout<<"last = "<<t<<endl;
+                string Rid;
+                stringstream ss;
+                ss << (t+1);
+                Rid="P"+ss.str();
+
+                cout<<"ID = "<<Rid<<endl; 
+                Type_list[t][0]=Rid;
+                cout<<"Name : "; cin>>Type_list[t][1];
+                cout<<endl;
+                cout<<"-------------------------------------------------------------------------------"<<endl;
+            // }
           }else if(sell_t=='E'){
             // EditType();
           }else if(sell_t=='D'){
@@ -50,38 +108,70 @@ void typeMenu(){
 
 // ================================Sale Menu==========================
 void sale(){
+      string This,ID;
       char sell_s;
       int s,s2;
+      const int columnWidth = 10;
       string Sale_list[100][2]={{"S1","P1"},
-                                   {"S2","P2"}
-                                  };
+                                {"S2","P2"}
+                               };
       int Price_list[100][3]={{5,2,10},
                               {5,2,10}
                           };
       do{
           headerTable();  //แสดง =======
-          cout<<setw(55)<<"----------Sale Menu----------"<<endl;
+          cout<<setw(55)<<"---------- Sale Menu ----------"<<endl;
           cout<<setw(54)<<"Press [L] to Show History"<<endl;
           cout<<setw(55)<<"Press [A] to Add Sale Page"<<endl;  
           cout<<setw(56)<<"Press [E] to Edit Sale page"<<endl;
           cout<<setw(58)<<"Press [D] to Delete Sale page"<<endl;
           footerReturn(); //แสดง =======
           cin>>sell_s;
+          //system("cls");
           sell_s = toupper(sell_s);
           if(sell_s=='L'){ 
-              cout<<"show_list_in S"<<endl;
+              cout<<setw(60)<<"--------------- History ---------------"<<endl;
+              cout<<"-------------------------------------------------------------------------------"<<endl;
+              cout<<setw(8)<<"ID"<<setw(12)<<"Name"<<setw(14)<<"Price"<<setw(13)<<"Amount"<<setw(13)<<"Total"<<endl;
+              cout<<"-------------------------------------------------------------------------------"<<endl;
 
                  for(int i=0;i<=5;i++){
                       for(int i2=0;i2<=2;i2++){
-                          cout<<Sale_list[i][i2]<<"   ";
+                          cout<<setw(columnWidth)<<Sale_list[i][i2]<<" | ";
                       }
                       for(int i2=0;i2<=1;i2++){
-                          cout<<Price_list[i][i2]<<"   ";
+                          cout<<setw(columnWidth)<<Price_list[i][i2]<<" | ";
                       }
                       cout<<endl;
                   }   
           }else if(sell_s=='A'){
-            // Addsale();
+            cout<<setw(60)<<"--------------- Add Sale ---------------"<<endl;
+            for(int m=0; m<100;m++){
+                for(int u=0; u<3;u++){
+                    // cout<<product[m][u]<<"   ";
+                    
+                    if(Sale_list[m][u]==""){
+                        break; //ข้ามไป
+                    } 
+                    s=m+1;
+                }
+                // cout<<endl;
+            }
+                cout<<"last = "<<s<<endl;
+                string Rid;
+                stringstream ss;
+                ss << (s+1);
+                Rid="P"+ss.str();
+
+                cout<<"ID = "<<Rid<<endl; 
+                Sale_list[s][0]=Rid;
+                cout<<"ID Product : "; cin>>Sale_list[s][1];
+                cout<<"Price : "; cin>>Price_list[s][0];
+                cout<<"Amount : "; cin>>Price_list[s][1];
+                cout<<"Total : "; cin>>Price_list[s][2];
+                cout<<endl;
+                cout<<"-------------------------------------------------------------------------------"<<endl;
+            // }
           }else if(sell_s=='E'){
             // Editsale();
           }else if(sell_s=='D'){
--- product.cpp
+++ product.cpp
@@ -1,6 +1,8 @@
 void product(){
+      string This,ID;
       char sell_p;
       int c,c2;
+      const int columnWidth = 10;
       string Product_list[100][3]={{"P1","Pen","1"},
                                    {"P2","Eraser","1"},
                                    {"P3","Pencil","1"},
@@ -31,7 +33,7 @@ void product(){
                           };
       do{
           headerTable();  //แสดง =======
-          cout<<setw(55)<<"----------Product Menu----------"<<endl;
+          cout<<setw(55)<<"---------- Product Menu ----------"<<endl;
           cout<<setw(57)<<"Press [L] to Show List Product"<<endl;
           cout<<setw(54)<<"Press [S] to Search Product"<<endl;
           cout<<setw(51)<<"Press [A] to Add Product"<<endl;  
@@ -41,22 +43,84 @@ void product(){
           cin>>sell_p;
           sell_p = toupper(sell_p);
           if(sell_p=='L'){ 
-              cout<<"show_list_in P"<<endl;
+              cout<<setw(60)<<"--------------- List Product ---------------"<<endl;
+              cout<<"-------------------------------------------------------------------------------"<<endl;
+              cout<<setw(8)<<"ID"<<setw(12)<<"Name"<<setw(13)<<"Type"<<setw(14)<<"Price"<<setw(13)<<"Amount"<<endl;
+              cout<<"-------------------------------------------------------------------------------"<<endl;
 
                  for(int i=0;i<=5;i++){
                       for(int i2=0;i2<=2;i2++){
-                          cout<<Product_list[i][i2]<<"   ";
+                          cout<<left<<setw(columnWidth)<<Product_list[i][i2]<<" | ";
                       }
                       for(int i2=0;i2<=1;i2++){
-                          cout<<Price_list[i][i2]<<"   ";
+                          cout<<left<<setw(columnWidth)<<Price_list[i][i2]<<" | ";
                       }
                       cout<<endl;
                   }
-            
-          }else if(sell_p=='S'){
-            //search
+            }else if(sell_p=='S'){
+            cout<<setw(65)<<"--------------- Search Product ---------------"<<endl;
+            cout<<"Enter ID : "; cin>>ID;
+
+
+            for(int m=0; m<100;m++){
+                for(int u=0; u<3;u++){
+                    if(Product_list[m][u]==""){
+                        break; //ข้ามไป
+                    } 
+                    c=m+1;
+                }
+                cout<<endl;
+            }
+            for(int i=0;i<c;i++){
+                for(int i2=0;i2<3;i2++){
+                    if(ID==Product_list[i][i2]){
+                        This=Product_list[i][i2];
+                        c=i; 
+                    }
+                }  
+            }  
+            if(This==""){
+                cout<<"Not Found!"<<endl;
+            }else{
+                    cout<<"ID : "<<Product_list[c][0]<<endl;
+                    cout<<"Name : "<<Product_list[c][1]<<endl;
+                    cout<<"Type : "<<Product_list[c][2]<<endl;
+                    cout<<"price : "<<Price_list[c][0]<<endl;
+                    cout<<"amount : "<<Price_list[c][1]<<endl;
+                    cout<<endl; 
+            }
+            This="";
+            cout<<"-------------------------------------------------------------------------------"<<endl;
+
           }else if(sell_p=='A'){
-            // AddProduct();
+            cout<<setw(60)<<"--------------- Add Product ---------------"<<endl;
+            for(int m=0; m<100;m++){
+                for(int u=0; u<3;u++){
+                    // cout<<product[m][u]<<"   ";
+                    
+                    if(Product_list[m][u]==""){
+                        break; //ข้ามไป
+                    } 
+                    c=m+1;
+                }
+                // cout<<endl;
+            }
+                cout<<"last = "<<c<<endl;
+                string Rid;
+                stringstream ss;
+                ss << (c+1);
+                Rid="P"+ss.str();
+
+                cout<<"ID = "<<Rid<<endl; 
+                Product_list[c][0]=Rid;
+                cout<<"Name : "; cin>>Product_list[c][1];
+                cout<<"Type : "; cin>>Product_list[c][2];
+                cout<<"Price : "; cin>>Price_list[c][0];
+                cout<<"Amount : "; cin>>Price_list[c][1];
+
+                cout<<endl;
+                cout<<"-------------------------------------------------------------------------------"<<endl;
+            // }
           }else if(sell_p=='E'){
             // EditProduct();
           }else if(sell_p=='D'){
