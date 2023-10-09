d292696012d3257d7e847e1ae6a81a70a6cc8b1a
YANiS <dominyl880@gmail.com>
2023-10-09 16:28:59 +0700

    sale

--- 1test.cpp
+++ 1test.cpp
@@ -26,7 +26,8 @@ int main(){
                         {1,9}
                       }; 
     string select,This,ID;
-    int c,beforeDel,k; //หาตำแหน่ง  , จำนวนข้อมูลก่อนลบ 
+    int c,beforeDel,k,amount,am; //หาตำแหน่ง  , จำนวนข้อมูลก่อนลบ , จำนวนขาย , จำนวนที่เพิ่มสินค้า
+    float cash,saletotal;
     char anw; //เช็ค Y N 
     string old; //เก็บไอดีเก่า
 
@@ -122,10 +123,8 @@ int main(){
                 cout<<"Type : "; cin>>product[c][2];
                 cout<<"Price : "; cin>>price[c][0];
                 cout<<"Amount : "; cin>>price[c][1];
-
                 cout<<endl;
-            // }
-
+                // }
         }else if(select=="E"){
             // =======================Edit=================================
             cout<<"Enter ID : "; cin>>ID;
@@ -300,50 +299,130 @@ int main(){
             This="";
             cout<<"========================================================="<<endl;
             
-        }else if(select=="K"){
-            // cout<<"===========================Sale=============================="<<endl;
-            cout<<"===========================SALE=============================="<<endl;
-            cout<<"==========================HISTORY============================"<<endl;
-            cout<<"-------------------------------------------------------------------------------"<<endl;
-            cout<<setw(8)<<"ID"<<setw(14)<<"ID PRODUCT"<<setw(8)<<"Name"<<setw(12)<<"Price"<<setw(12)<<"Amount"<<setw(10)<<"Total"<<endl;
-            cout<<"-------------------------------------------------------------------------------"<<endl;
-                for(int m=0; m<100;m++){
-                    for(int u=0; u<2;u++){
-                        if(sale[m][u]==""){
-                            break; //ข้ามไป
-                        } 
-                        c=m+1;
-                    }
-                }
-
-                for(int i=0;i<c;i++){
-                    cout<<setw(8)<<sale[i][0]<<" | ";
-                    cout<<setw(8)<<sale[i][1]<<" | ";
-                    for(int i2=0; i2<100;i2++){
-                        // cout<<setw(8)<<product[i2][0]<<" | ";
-                        if(sale[i][1]==product[i2][0]){
-                            cout<<setw(8)<<product[i2][1]<<" | ";
-                            cout<<setw(8)<<price[i2][1]<<" | ";
-
+        }else if(select=="K"){                
+                // cout<<"===========================Sale=============================="<<endl;
+                cout<<"===========================SALE pass X to exit=============================="<<endl;
+                cout<<"==========================HISTORY============================"<<endl;
+                cout<<"-------------------------------------------------------------------------------"<<endl;
+                cout<<setw(8)<<"ID"<<setw(14)<<"ID PRODUCT"<<setw(8)<<"Name"<<setw(12)<<"Price"<<setw(12)<<"Amount"<<setw(10)<<"Total"<<endl;
+                cout<<"-------------------------------------------------------------------------------"<<endl;
+                    for(int m=0; m<100;m++){
+                        for(int u=0; u<2;u++){
+                            if(sale[m][u]==""){
+                                break; //ข้ามไป
+                            } 
+                            c=m+1;
                         }
                     }
 
-                    cout<<setw(8)<<sale_total[i][0]<<" | ";
-                    cout<<setw(8)<<sale_total[i][1]<<" | ";
-                     
-                    cout<<endl;
-                    cout<<endl;
-                } 
+                    for(int i=0;i<c;i++){
+                        cout<<setw(8)<<sale[i][0]<<" | ";
+                        cout<<setw(8)<<sale[i][1]<<" | ";
+                        for(int i2=0; i2<100;i2++){
+                            // cout<<setw(8)<<product[i2][0]<<" | ";
+                            if(sale[i][1]==product[i2][0]){
+                                cout<<setw(8)<<product[i2][1]<<" | ";
+                                cout<<setw(8)<<price[i2][1]<<" | ";
+                            }
+                        }
 
-                cout<<"Enter Sale : "; cin>>ID;
-                cout<<"========================================================="<<endl;
+                        cout<<setw(8)<<sale_total[i][0]<<" | ";
+                        cout<<setw(8)<<sale_total[i][1]<<" | ";
+                        
+                        cout<<endl;
+                        cout<<endl;
+                    } 
+ 
+                                    // เพิ่มการขาย
+                            do{
+                                cout<<"Enter Product ID : "; cin>>ID;
+                                        for(int i=0;i<100;i++){
+                                            // cout<<product[i][0]<<endl;
+                                            if(ID==product[i][0]){
+                                                anw='Y';
+                                                break;
+                                            }else{
+                                                anw='N';
+                                            }
 
+                                        }
+                            }while(anw!='Y');
+
+                            do{
+                                cout<<"Enter Amount : "; cin>>amount;
+                                for(int i=0;i<100;i++){
+                                        if(ID==product[i][0]){
+                                            if(amount>price[i][0]){
+                                                cout<<"Over limit! Try again."<<endl;
+                                                amount=-1;
+                                                break;
+                                            }
+                                        }
+                                    }
+                            }while(amount==-1);
+                                cout<<"------------Sale Slip---------"<<endl;
+                                cout<<"Product ID = "<<ID<<endl;
+                                for(int i=0;i<100;i++){
+                                    if(ID==product[i][0]){
+                                        cout<<"Product Name = "<<product[i][1]<<endl;
+                                        cout<<"Product Price = "<<price[i][1]<<endl;
+                                        cout<<"Amount = "<<amount<<endl;
+                                        if(amount>price[i][0]){
+                                            cout<<"Over limit! Try again."<<endl;
+                                            amount=-1;
+                                            break;
+                                        }
+                                        saletotal=amount*price[i][1];
+                                        cout<<"Total cash = "<<saletotal<<endl;
+                                        do{
+                                            cout<<"Enter cash : "; cin>>cash;
+                                            if((cash-(amount*price[i][1]))<0){
+                                                anw='N';
+                                            }else{
+                                                anw='Y';
+                                            }
+                                        }while(anw!='Y');
+                                        cout<<"Change cash = "<<cash-(amount*price[i][1])<<endl;
+                                    }
+                                }
+                                    cout<<"------------Sale Slip---------"<<endl;
+                                    // update to array price and insert to sale sale_total 
+                                        for(int i=0;i<100;i++){
+                                            // cout<<ID<<endl;
+                                            // cout<<product[i][0]<<endl;
+                                            if(ID==product[i][0]){
+                                                price[i][0]=price[i][0]-amount;
+                                                break;
+                                            }
+                                        }
+                                    // =======================Add=================================
+                                        for(int m=0; m<100;m++){
+                                            for(int u=0; u<2;u++){
+                                                if(sale[m][u]==""){
+                                                    break; //ข้ามไป
+                                                } 
+                                                c=m+1;
+                                            }
+                                        }
+                                        // cout<<"last = "<<c<<endl;
+                                        string Rid;
+                                        stringstream ss;
+                                        ss << (c+1);
+                                        Rid="S"+ss.str();
+                        
+                                        // cout<<"ID sale = "<<Rid<<endl; 
+                                        sale[c][0]=Rid;
+                                        sale[c][1]=ID;
+                                        sale_total[c][0]=amount;
+                                        sale_total[c][1]=saletotal;
+                        
+                                        cout<<endl;
+
+                          
+
+                            cout<<"========================================================="<<endl;
+                            
         }
 
     }while(select!="x");
-
-
-    
-
-
 }
